b825a1f1b128dd64763dd5c0870426d192f298fb
LucasGA10 <laramayo752@alumno.unlam.edu.ar>
2025-10-19 15:50:23 -0300

    Event models

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/model/EventEntity.kt
@@ -0,0 +1,17 @@
+package ar.edu.unlam.mobile.scaffolding.data.model
+
+data class EventEntity(
+    val eventId: String,
+    val title: String,
+    val description: String,
+    val dateTime: Long,
+    val lat: Double,
+    val lng: Double,
+    val imageUrl: String?,
+    val beforeImageUrl: List<String>,
+    val afterImageUrl: List<String>?,
+    val membersId: List<Int>?,
+    val creatorId: Int,
+    val saved: Boolean,
+    val participating: Boolean,
+)
